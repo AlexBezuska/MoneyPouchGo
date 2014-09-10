682378c0d3f03db851bea2de5b0ef1722d6cdd9d
Alex Bezuska <abezuska@gmail.com>
2014-09-10 10:39:08 -0400

    add transaction type interface, click to choose working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-main.js
+"use strict";
+
+$(document).ready(function() {
+	$(document).on("mousedown touchstart", ".type div", function() {
+		deactivateAllCategories();
+		activateCategory(this);
+		setFocus(".amount");
+	});
+});
+
+function deactivateAllCategories() {
+	$(".type .active").removeClass("active");
+}
+
+function activateCategory(element) {
+	$(element).addClass("active");
+}
+
+function setFocus(element) {
+	$(element).focus();
+}
