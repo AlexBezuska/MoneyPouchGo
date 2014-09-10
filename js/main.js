761e0c585ab18ed3586f99352742d9e1194c5b31
Alex Bezuska <abezuska@gmail.com>
2014-09-10 10:57:20 -0400

    add JavaScript to keep money in the right format at all times

--- js/main.js
+++ js/main.js
@@ -6,6 +6,25 @@ $(document).ready(function() {
 		activateCategory(this);
 		setFocus(".amount");
 	});
+
+	$('*[data-mask="money"]').each(function() {
+		var value = $(this).val().replace("$", "").replace(",", "");
+		if (isNaN(value) || value == "") {
+			value = "0.00";
+		}
+		value = toMoney(value);
+		$(this).val(value);
+	});
+
+	$("*[data-mask='money']").click(function() {
+		$(this).select();
+	});
+
+	$('*[data-mask="money"]').focus(function() {}).blur(function() {
+		var value = $(this).val().replace("$", "").replace(",", "");
+		value = toMoney(value);
+		$(this).val(value);
+	});
 });
 
 function deactivateAllCategories() {
@@ -18,4 +37,48 @@ function activateCategory(element) {
 
 function setFocus(element) {
 	$(element).focus();
+}
+
+function addCommas(str) {
+	str += '';
+	var x = str.split('.');
+	var x1 = x[0];
+	var x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+
+function toMoney(value) {
+	var value = value.replace(/[^0-9.-]/g, "");
+	var valueLength = value.length - 1;
+	if (value.indexOf(".") !== -1) {
+		if (value.substring(value.indexOf("."), valueLength).length == 0) {
+			value = "$" + value + "00";
+			return addCommas(value);
+		}
+		if (value.substring(value.indexOf("."), valueLength).length == 1) {
+			value = "$" + value + "0";
+			return addCommas(value);
+		}
+		if (value.substring(value.indexOf("."), valueLength).length == 2) {
+			value = "$" + value;
+			return addCommas(value);
+		} else {
+			var value = value.replace(/[^0-9.-]/g, "");
+			value = preciseRound(value);
+			value = "$" + value.toString();
+			return addCommas(value);
+		}
+	} else {
+		value = "$" + value + ".00";
+		return addCommas(value);
+	}
+}
+
+function preciseRound(num, decimals) {
+	var rounded = Math.round(num * Math.pow(10, decimals)) / Math.pow(10, decimals);
+	return rounded;
 }
